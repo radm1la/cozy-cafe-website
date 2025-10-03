2a7633d172bb4caf7fc302c2c00b7f022f37fcca
CodeRadmila <radmilashamilova12@gmail.com>
2025-10-03 15:18:53 +0400

    finished(for now) Menu page with responsiveness

--- script_menu.js
+++ script_menu.js
@@ -1,21 +1,22 @@
 const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".content");
+const reserve_btn = document.querySelector(".reserve_btn");
+
 
 tabs.forEach(tab => {
   tab.addEventListener("click", () => {
-    // Remove active from all tabs
     tabs.forEach(t => t.classList.remove("active"));
 
-    // Hide all content divs (#drinks, #food, #desserts)
-    document.querySelectorAll("#drinks, #food").forEach(c => {
-      c.classList.remove("active_content");
-    });
-
-    // Activate clicked tab
     tab.classList.add("active");
 
-    // Get tab text, lowercase it, and show matching content
-    const idToShow = tab.textContent.toLowerCase(); 
-    const contentDiv = document.getElementById(idToShow);
-    contentDiv.classList.add("active_content");
+    contents.forEach(c => c.classList.remove("active_content"));
+    const active_content = document.getElementById(tab.innerHTML.toLowerCase());
+    active_content.classList.add("active_content");
+    
   });
+});
+
+
+reserve_btn.addEventListener("click",()=>{
+  window.location.href = "contact.html";
 });
